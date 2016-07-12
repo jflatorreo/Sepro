bbd0486104d03e8706220642b627c8c04ae3e024
jflatorreo <jflatorreo@unal.edu.co>
2016-07-12 12:20:27 -0500

    Cabecera equipo

--- equipo.php
+++ equipo.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-header('Content-Type: text/html; charset=utf8');
-?>
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
    <head>
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -16,13 +14,13 @@ header('Content-Type: text/html; charset=utf8');
       <meta http-equiv="last-modified" content=""/>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
       <link href="css/allCSS.css" rel="stylesheet" type="text/css" />
-      <link href="css/newstyleverde2.css" rel="stylesheet" type="text/css" />
+      <link href="css/newstyle.css" rel="stylesheet" type="text/css" />
 	  <script type="text/javascript" src="js/jssor.slider.min.js"></script>
       <title>Universidad Nacional de Colombia</title>
    <body>
       <div id="contenedor_info">
 	  <?php include_once 'scripts/cabeceraUnal.html';
-	       include_once 'scripts/cabeceraSepro.html';
+			include_once 'scripts/cabeceraSepro.html';
       ?>
       <div>
 	 <iframe style="width: 100%;min-height: 500px " src="
